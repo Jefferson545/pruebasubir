77a7094725d27782bb1c58f582816c9faf33a614
jefferson <jefferson.avelar@catolica.edu.sv>
2023-03-03 22:20:55 -0600

    Agregar archivos de proyecto.

---
+++ LabNathalyMatute/Form1.Designer.cs
@@ -0,0 +1,264 @@
+﻿namespace LabNathalyMatute
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.txtfila = new System.Windows.Forms.TextBox();
+            this.txtcolumna = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtfilab = new System.Windows.Forms.TextBox();
+            this.txtcolumnab = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.button4 = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(739, 14);
+            this.button1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(111, 31);
+            this.button1.TabIndex = 8;
+            this.button1.Text = "Generar";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // txtfila
+            // 
+            this.txtfila.Location = new System.Drawing.Point(468, 84);
+            this.txtfila.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.txtfila.Name = "txtfila";
+            this.txtfila.Size = new System.Drawing.Size(157, 22);
+            this.txtfila.TabIndex = 10;
+            // 
+            // txtcolumna
+            // 
+            this.txtcolumna.Location = new System.Drawing.Point(468, 127);
+            this.txtcolumna.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.txtcolumna.Name = "txtcolumna";
+            this.txtcolumna.Size = new System.Drawing.Size(157, 22);
+            this.txtcolumna.TabIndex = 11;
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.Location = new System.Drawing.Point(739, 49);
+            this.button2.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(111, 31);
+            this.button2.TabIndex = 14;
+            this.button2.Text = "Limpiar";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button3.Location = new System.Drawing.Point(739, 127);
+            this.button3.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(111, 31);
+            this.button3.TabIndex = 15;
+            this.button3.Text = "Salir";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.label2.Location = new System.Drawing.Point(340, 135);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(79, 16);
+            this.label2.TabIndex = 13;
+            this.label2.Text = "Columnas:";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.label1.Location = new System.Drawing.Point(340, 92);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(45, 16);
+            this.label1.TabIndex = 12;
+            this.label1.Text = "Filas:";
+            // 
+            // txtfilab
+            // 
+            this.txtfilab.Location = new System.Drawing.Point(1105, 89);
+            this.txtfilab.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.txtfilab.Name = "txtfilab";
+            this.txtfilab.Size = new System.Drawing.Size(157, 22);
+            this.txtfilab.TabIndex = 16;
+            // 
+            // txtcolumnab
+            // 
+            this.txtcolumnab.Location = new System.Drawing.Point(1105, 130);
+            this.txtcolumnab.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.txtcolumnab.Name = "txtcolumnab";
+            this.txtcolumnab.Size = new System.Drawing.Size(157, 22);
+            this.txtcolumnab.TabIndex = 17;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.label3.Location = new System.Drawing.Point(968, 92);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(45, 16);
+            this.label3.TabIndex = 18;
+            this.label3.Text = "Filas:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.label4.Location = new System.Drawing.Point(968, 139);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(79, 16);
+            this.label4.TabIndex = 19;
+            this.label4.Text = "Columnas:";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.label5.Location = new System.Drawing.Point(500, 36);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(62, 16);
+            this.label5.TabIndex = 20;
+            this.label5.Text = "Matriz A";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.label6.Location = new System.Drawing.Point(1143, 36);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(62, 16);
+            this.label6.TabIndex = 21;
+            this.label6.Text = "Matriz B";
+            // 
+            // button4
+            // 
+            this.button4.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button4.Location = new System.Drawing.Point(739, 89);
+            this.button4.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(111, 31);
+            this.button4.TabIndex = 22;
+            this.button4.Text = "Multiplicar";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.button4);
+            this.groupBox1.Controls.Add(this.label6);
+            this.groupBox1.Controls.Add(this.label5);
+            this.groupBox1.Controls.Add(this.label4);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.txtcolumnab);
+            this.groupBox1.Controls.Add(this.txtfilab);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.button3);
+            this.groupBox1.Controls.Add(this.button2);
+            this.groupBox1.Controls.Add(this.txtcolumna);
+            this.groupBox1.Controls.Add(this.txtfila);
+            this.groupBox1.Controls.Add(this.button1);
+            this.groupBox1.Location = new System.Drawing.Point(5, 4);
+            this.groupBox1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Padding = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.groupBox1.Size = new System.Drawing.Size(1976, 167);
+            this.groupBox1.TabIndex = 15;
+            this.groupBox1.TabStop = false;
+            // 
+            // panel1
+            // 
+            this.panel1.Location = new System.Drawing.Point(5, 176);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(1873, 739);
+            this.panel1.TabIndex = 16;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.WhiteSmoke;
+            this.ClientSize = new System.Drawing.Size(1892, 927);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.groupBox1);
+            this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.Name = "Form1";
+            this.Text = "Laboratorio Nathaly Matute";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox txtfila;
+        private System.Windows.Forms.TextBox txtcolumna;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtfilab;
+        private System.Windows.Forms.TextBox txtcolumnab;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Panel panel1;
+    }
+}
+
---
+++ LabNathalyMatute/Form1.cs
@@ -0,0 +1,208 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Globalization;
+using System.Linq;
+using System.Reflection.Emit;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Windows.Forms.VisualStyles;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+using TextBox = System.Windows.Forms.TextBox;
+namespace LabNathalyMatute
+{
+    public partial class Form1 : Form
+    {
+
+        public Form1()
+        {
+            InitializeComponent();
+            //label9.Enabled = false;
+           
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+            try
+            {
+                int fil = Convert.ToInt32(txtfila.Text);
+                int colum = Convert.ToInt32(txtcolumna.Text);
+
+                matriz1 = new TextBox[fil, colum];
+                for (int a = 0; a < fil; a++)
+                {
+                    for (int b = 0; b < colum; b++)
+                    {
+                        matriz1[a, b] = new TextBox();
+                        matriz1[a, b].Text = "";
+                        matriz1[a, b].Width = 80;
+                        matriz1[a, b].BackColor = Color.AliceBlue;
+                        matriz1[a, b].Location = new Point(50 + 100 * b, 35 + 50 * a);
+                        matriz1[a, b].TabIndex = 100 * a + 2 * b;
+
+                        panel1.Controls.Add(matriz1[a, b]);
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            try
+            {
+                int filB = int.Parse(txtfilab.Text);
+                int columB = int.Parse(txtcolumnab.Text);
+
+                matriz2 = new TextBox[filB, columB];
+                for (int a = 0; a < filB; a++)
+                {
+                    for (int b = 0; b < columB; b++)
+                    {
+                        matriz2[a, b] = new TextBox();
+                        matriz2[a, b].Text = "";
+                        matriz2[a, b].Width = 80;
+                        matriz2[a, b].BackColor = Color.AliceBlue;
+                        matriz2[a, b].Location = new Point(70 + 70 * (columB * 2) + 100 * b, 50 + 50 * (filB + 2) + 50 * a);
+                        matriz2[a, b].TabIndex = 80 * a + 2 * b;
+
+                        panel1.Controls.Add(matriz2[a, b]);
+                    }
+                }
+                
+
+            }
+
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            MessageBox.Show("Se han generado las matrices correctamente", "Exito", MessageBoxButtons.OK);
+           
+
+
+        }
+        int[,] mult;
+        TextBox[,] matriz1;
+        TextBox[,] matriz2;
+        TextBox[,] mt;
+        public static TextBox matrizresultado;
+
+        public void generarMatriz()
+        {
+            
+
+
+        }
+        public void multiplicar()
+        {
+            
+
+
+
+
+        }
+
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            txtfila.Clear();
+            txtcolumna.Clear();
+            txtfilab.Clear();
+            txtcolumnab.Clear();
+            panel1.Controls.Clear();
+            
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            mult = new int[100, 100];
+            mt = new TextBox[1000, 1000];
+            int filas1 = matriz1.GetLength(0);
+            int columnas1 = matriz1.GetLength(1);
+            int filas2 = matriz2.GetLength(0);
+            int columnas2 = matriz2.GetLength(1);
+            int colu, fili;
+            colu = int.Parse(txtcolumna.Text);
+            fili = int.Parse(txtfilab.Text);
+
+            if (colu == fili)
+            {
+                for (int a = 0; a < filas1; a++)
+                {
+                    for (int b = 0; b < columnas2; b++)
+                    {
+                        mult[a, b] = 0;
+                        for (int c = 0; c < columnas1; c++)
+                        {
+                            mult[a, b] = mult[a, b] + (int.Parse(matriz2[a, c].Text) * int.Parse(matriz1[c, b].Text));
+                        }
+                        matrizresultado = new TextBox();
+                        mt[a, b] = matrizresultado;
+                        panel1.Controls.Add(mt[a, b]);
+
+                        matrizresultado.Top = 200 + (a * 30);
+                        matrizresultado.Left = 100 + (b * 50);
+                        matrizresultado.BackColor = Color.AliceBlue;
+                        matrizresultado.Height = 25;
+                        matrizresultado.Width = 25;
+
+                        matrizresultado.Name = "c" + (a + 1) + (b + 1);
+                        mt[a, b].Text = Convert.ToString(mult[a, b]);
+
+
+
+                    }
+                }
+
+                for (int a = 0; a < filas1; a++)
+                {
+                    for (int b = 0; b < columnas2; b++)
+                    {
+                        mult[a, b] = 0;
+                        for (int c = 0; c < columnas1; c++)
+                        {
+                            mult[a, b] = mult[a, b] + (int.Parse(matriz1[a, c].Text) * int.Parse(matriz2[c, b].Text));
+                        }
+                        matrizresultado = new TextBox();
+                        mt[a, b] = matrizresultado;
+                        panel1.Controls.Add(mt[a, b]);
+
+                        matrizresultado.Top = 50 + (a * 30);
+                        matrizresultado.Left = 350 + (b * 50);
+                        matrizresultado.BackColor = Color.AliceBlue;
+                        matrizresultado.Height = 25;
+                        matrizresultado.Width = 25;
+
+                        matrizresultado.Name = "c" + (a + 1) + (b + 1);
+                        mt[a, b].Text = Convert.ToString(mult[a, b]);
+
+
+
+                    }
+                }
+
+
+
+            }
+            else
+            {
+                MessageBox.Show("la columna de la matriz A tiene que ser igual a fila de la matriz B ", "Exito", MessageBoxButtons.OK);
+            }
+           
+        }
+        private void label7_Click(object sender, EventArgs e)
+        {
+            
+        }
+    }
+    }
+
